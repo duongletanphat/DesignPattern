dc8bf75eb9e51d4b2b0e78feb840ff3166271250
Phat Duong <phatduong80@gmail.com>
2022-05-03 10:21:57 +0700

    Main project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace testCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ->>> ADAPTER
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            System.Console.WriteLine("Adaptee interface is incompatible with the client.");
+            System.Console.WriteLine("But with adapter client can call it's method");
+
+            System.Console.WriteLine(target.GetRequest());
+            System.Console.WriteLine();
+
+            //========================================================
+
+            //->> Abstract Factory
+            new Client().Main();
+        }
+    }
+}
